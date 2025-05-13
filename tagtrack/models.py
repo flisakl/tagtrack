628364194ad4ffeddcde846485d268190b331956
Łukasz Flisak <lu.dev00@gmail.com>
2025-05-13 23:00:08 +0200

    Add Album model

--- tagtrack/models.py
+++ tagtrack/models.py
@@ -7,3 +7,23 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=200)
+    # Fields below may be blank, because there is no guarantee that uploaded
+    # files will hold any metadata
+    image = models.ImageField(upload_to='albums', null=True, blank=True)
+    genre = models.CharField(max_length=50, null=True, blank=True)
+    year = models.PositiveIntegerField(null=True, blank=True)
+
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    class Meta:
+        # This ensures we won't be able to create 2 albums with the same name
+        # for the same artist
+        constraints = [
+            models.constraints.UniqueConstraint(
+                fields=['artist_id', 'name'], name='unique_artist_album'
+            )
+        ]
