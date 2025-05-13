51ef3f0efdb1048609e6f24dd710bbe4d23a044c
Łukasz Flisak <lu.dev00@gmail.com>
2025-05-13 22:29:12 +0200

    Add Artist model

--- tagtrack/models.py
+++ tagtrack/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='artists', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
